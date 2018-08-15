db5b2967b839e219932d0160e088efd6aee2ec75
nima <ni_moradi@yahoo.com>
2018-08-15 08:58:35 +0430

    adding more methods

--- app/src/main/java/com/stfalcon/chatkit/sample/prototype/dialogProto.java
+++ app/src/main/java/com/stfalcon/chatkit/sample/prototype/dialogProto.java
@@ -3,6 +3,7 @@ package com.stfalcon.chatkit.sample.prototype;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.stfalcon.chatkit.sample.common.data.model.Dialog;
+import com.stfalcon.chatkit.sample.common.data.model.User;
 import com.stfalcon.chatkit.sample.responseModel.LoginData;
 import com.stfalcon.chatkit.sample.responseModel.dialogData;
 import com.stfalcon.chatkit.sample.staticData;
@@ -22,6 +23,17 @@ public interface dialogProto {
             "Content-Type: application/json"})
     @POST("dialog/getDialog?")
     Call<List<Dialog>> getDialogs(@Query("uid") String uid, @Query("timestamp") String time);
+
+    @Headers({"Accept: application/json",
+            "Content-Type: application/json"})
+    @POST("dialog/getAllChats?")
+    Call<List<User>> getAllChats(@Query("uid") String uid);
+
+    @Headers({"Accept: application/json",
+            "Content-Type: application/json"})
+    @POST("dialog/newDialog?")
+    Call<Dialog> newDialog(@Query("uid") String uid, @Query("timestamp") String time,
+                           @Query("newDialog")  Dialog newDialog);
 
     Gson gson = new GsonBuilder()
             .setDateFormat("yyyy-MM-dd'T'HH:mm:ss")
